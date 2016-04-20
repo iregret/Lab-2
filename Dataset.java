f6586c6336902958df42511ef29209a6fd2c8fc4
iregret <larryjustineckert@me.com>
2016-04-19 21:17:05 -0700

    Working Dataset.java
    Lots of things changed in this class. While discussing it online and with peers at school, it was suggested to use a 2 dimensional array to step through the file and then create a method for printing the results. This is a slightly different approach than what I had originally considered. By using a for loop, I could step through the array and print just a particular element(or is it index?). The first method scans through the text file and prints out one at a time, the second method is a for loop for totaling a particular index. That is returned to the main class.

--- Dataset.java
+++ Dataset.java
@@ -1,26 +1,31 @@
-import java.awt.*;
-import java.io.*;
-import java.util.*;
-
-
 public class Dataset
 {
-	String first, last, address, city, state, phone;
-	int zip;
-	double donation;
+	private String [][] data;
+	private int row;
+	private double total=0;
+	private double l;
+	private String col9;
 
-
-	public void addData(String f, String l, String a, String c, String s, String p, int z, double d)
+	public void print(String [] [] data, int row)
 	{
-
-		first = f;
-		last = l;
-		address = a;
-		city = c;
-		state = s;
-		phone = p;
-		zip = z;
-		donation = d;
+		for (int count= 0; count < row; count++)
+			{
+				System.out.println(data [count][0] + ' ' + data[count][1]);
+				System.out.println(data [count][2] + ' ' + data[count][3] + ' ' + data[count][4]);
+				System.out.println(data [count][5] + ' ' + data[count][6] + ' ' + data[count][7]);
+				System.out.println(data [count][8] + ' ' + data[count][9]);
+				System.out.println("\n");
+			}
+			
+	}		
+	public double donationTotal(String [] [] data, int row)
+	{
+		for (int i=0; i<row; i++)
+		{
+		col9 = data[i][9];
+		l = Double.parseDouble(col9);
+		total += l;
+		}
+	return total;
 	}
-
 }
